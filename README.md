33a145b4219af5ce159d4f85e5af9ea757a57ae4
- <74578072+darmawan06@users.noreply.github.com>
2022-06-09 04:15:30 -0700

    Build

--- README.md
+++
@@ -1,5 +0,0 @@
-# my-portfolio
- 
- 1 month to create a personal website 
- 
- 1/06/2022 - 30/06/2022
--- src/scripts/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import homepage from '../views/pages/homepage';
-import portfolio from '../views/pages/portfolio';
-
-const routes = {
-    '/': homepage,
-    '/portfolio': portfolio,
-};
-
-export default routes;
