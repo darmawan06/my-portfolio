5f4208a64d5655a4aac3a7384ae15d7480a62bb5
- <74578072+darmawan06@users.noreply.github.com>
2022-06-09 04:00:38 -0700

    build

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,55 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const tailwindcss = require('tailwindcss');
 const webpack = require("webpack");
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/scripts/index.js'),
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-        rules: [{
-                test: /\.css$/i,
-                use: [
-                  {
-                    loader: 'style-loader',
-                  },
-                  {
-                    loader: 'css-loader',
-                  },
-                  {
-                    loader: 'postcss-loader',
-                  }
-                ],
-            },
-            {
-              test: /\.(png|svg|jpg|jpeg|gif)$/,
-              use: [
-                  'file-loader',
-              ],
-            },
-        ]
+module.exports = (env) => {
+  return {
+    entry: path.resolve(__dirname, 'src/scripts/index.js'),
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src/templates/index.html"),
-        filename: "index.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
-  ],
-  mode : "development"
+    module: {
+          rules: [{
+                  test: /\.css$/i,
+                  use: [
+                    {
+                      loader: 'style-loader',
+                    },
+                    {
+                      loader: 'css-loader',
+                    },
+                    {
+                      loader: 'postcss-loader',
+                    }
+                  ],
+              },
+              {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+              },
+          ]
+      },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, "src/templates/index.html"),
+          filename: "index.html",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/public/'),
+            to: path.resolve(__dirname, 'dist/'),
+            noErrorOnMissing: true,
+          },
+        ],
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+      }),
+    ],
+    mode : env.mode
+  }
 };
