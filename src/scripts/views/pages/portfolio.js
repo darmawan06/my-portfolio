5fcbb94a27a4693e807d02561f94a8e8fee6b213
- <74578072+darmawan06@users.noreply.github.com>
2022-07-02 00:26:05 -0700

    x

--- src/scripts/views/pages/portfolio.js
+++ src/scripts/views/pages/portfolio.js
@@ -1,6 +1,7 @@
+
 const portfolio = {
 	async init(){
-		this.i = 0;
+		this.i = 3;
 		return `
 		<div class="w-full h-screen bg-white flex-col flex">
 			<div class="flex flex-row w-full h-full relative overflow-x-none">
@@ -17,10 +18,55 @@ const portfolio = {
 			</div>
 		</div>`;
 	},
+	sectionAbout(aboutMe){
+		if(aboutMe.type == "description"){
+			return `
+			<h3 class="break-word text-md text-justify font-h3 text-white h-auto max-h-3/6 max-rows">
+				${aboutMe.description}
+			</h3>`
+		}else if(aboutMe.type == 'list'){
+			let LITemp = ``;
+			aboutMe.list.forEach((e)=>{
+				LITemp += `
+					<li class="w-full p-2 my-2 border border-2 border-color-4">
+						<h3 class="text-md font-semibold text-white">${e.title}</h3>
+						<h4 class="text-sm text-white">${e.date} || ${e.desc}</h4> 
+					</li>
+				`;
+			})
+			return `
+				<ul class="w-full flex flex-col ">
+					${LITemp}
+				</ul>
+			`;
+		}else if(aboutMe.type == 'list-in-list'){
+			let LITemp = ``;
+			aboutMe.listInList.forEach((e)=>{
+				let LITempSkill = ``;
+				console.log(e)
+				e.skills.forEach((x)=>{
+					LITempSkill += `<li class="bg-color-4 text-color-2 text-center m-2 px-2 ">${x}</li>`;
+				})
+				LITemp += `
+					<li class="w-full p-2 my-2 border border-2 border-color-4">
+						<h3 class="text-md font-semibold text-white">${e.title}</h3>
+						<ul class="w-full flex flex-row overflow-x-auto scrollModification">
+							${LITempSkill}
+						</ul>
+					</li>
+				`;
+			})
+			return `
+				<ul class="w-full flex flex-col">
+					${LITemp}
+				</ul>
+			`;
+		}
+	},
 	sectionAboutMe(i){
 		let one,two,three,four;
 		const backToZero = (i)=>{			
-			if(i > 3){
+			if(i > this.dataAboutMe().length-1){
 				i=0;
 				return i;
 			}else{
@@ -31,12 +77,11 @@ const portfolio = {
 		two = backToZero(one+1);
 		three = backToZero(two+1);
 		four = backToZero(three+1);
+		console.log(`${one} - ${two} - ${three} - ${four}`)
 		return `			
 				<div class="row-span-full bg-color-3 p-8 flex flex-col">
-					<h1 class="text-[75px] text-color-4 font-h1 uppercase h-3/6">${this.dataAboutMe()[one].title}</h1>
-					<h3 class="break-all text-md text-justify font-h3 text-white h-auto max-h-2/6 max-rows">
-						${this.dataAboutMe()[one].description}
-					</h3>
+					<h1 class="text-[4.5vw] text-color-4 font-h1 uppercase h-auto">${this.dataAboutMe()[one].title}</h1>
+					${this.sectionAbout(this.dataAboutMe()[one])}
 					<div class="mt-auto ml-auto h-1/6 flex w-full">
 						<button class="next ml-auto mt-auto border border-4 border-color-4 p-2  shadow-inner">
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-color-4 shadow-inner" viewBox="0 0 20 20" fill="currentColor">
@@ -48,10 +93,7 @@ const portfolio = {
 				<div class="col-span-1 bg-color-2 flex">
 					<div class="w-2/6"></div>
 					<div class="w-4/6 flex flex-col">
-						<h1 class="text-[20px] mt-auto text-color-4 font-h1 uppercase">${this.dataAboutMe()[two].title}</h1>
-						<h3 class="text-[15px] mb-auto text-color-4 font-h3 truncate pr-4">
-							${this.dataAboutMe()[two].description}
-						</h3>
+						<h1 class="text-[20px] my-auto text-color-4 font-h1 uppercase">${this.dataAboutMe()[three].title}</h1>
 					</div>
 				</div>
 				<div class="col-span-1 bg-color-3 flex">
@@ -63,19 +105,13 @@ const portfolio = {
 						</button>
 					</div>
 					<div class="w-4/6 flex flex-col">
-						<h1 class="text-[20px] mt-auto text-color-4 font-h1 uppercase">${this.dataAboutMe()[three].title}</h1>
-						<h3 class="text-[15px] mb-auto text-color-4 font-h3 truncate pr-4">
-							${this.dataAboutMe()[three].description}
-						</h3>
+						<h1 class="text-[20px] my-auto text-color-4 font-h1 uppercase">${this.dataAboutMe()[two].title}</h1>
 					</div>
 				</div>
 				<div class="col-span-1 bg-color-2 flex">
 					<div class="w-2/6"></div>
 					<div class="w-4/6 flex flex-col">
-						<h1 class="text-[20px] mt-auto text-color-4 font-h1 uppercase">${this.dataAboutMe()[four].title}</h1>
-						<h3 class="text-[15px] mb-auto text-color-4 font-h3 truncate pr-4">
-							${this.dataAboutMe()[four].description}
-						</h3>
+						<h1 class="text-[20px] my-auto text-color-4 font-h1 uppercase">${this.dataAboutMe()[four].title}</h1>
 					</div>
 				</div>
 		`
@@ -111,7 +147,7 @@ const portfolio = {
 	},
 	async clickButton(eButton){
 		await $(eButton).on('click',()=>{
-			this.i = (this.i > 3) ? 0 : this.i;
+			this.i = (this.i > this.dataAboutMe().length -1) ? 0 : this.i;
 			document.querySelector('.sectionAboutMe').innerHTML = this.sectionAboutMe(this.i);
 			this.hoverButton('.next');
 			this.hoverButton('.back');
@@ -123,29 +159,53 @@ const portfolio = {
 	dataAboutMe(){
 		return [
 		{
-			title : "I'm Freelance",
-			description : `I have been freelance Web Development for about 6 months.
-			My Goal to become a freelance is to try a project that i have never tried and i 
-			do not think too much about the cost that gives because it has been paid with
-			experience. I feel happy and happy even though sometimes i feel tired.`
+			title : "Tentang Saya",
+			type : 'description',
+			description : `Saya adalah seorang Mahasiswa Ilmu Komputer (S1) di Universitas Pendidikan Indonesia, Bandung, Indonesia. Saya memiliki pengalaman Freelance Web Developer 1 tahun dan memiliki keahlian pengembangan website front-end dan back- end, Proyek yang sering saya kerjakan menggunakan bahasa pemograman PHP dengan framework CodeIgniter 4. Saya juga mengikuti kegiatan MBKM di PT Presetologics (Dicoding Indonesia) dengan mengikuti kelas FEBE (Front-End dan Back End).`
 		},{
-			title : "I'm Student",
-			description : `I have been freelance Web Development for about 6 months.
-			My Goal to become a freelance is to try a project that i have never tried and i 
-			do not think too much about the cost that gives because it has been paid with
-			experience. I feel happy and happy even though sometimes i feel tired.`			
+			title : "Pendidikan",
+			type : 'list',
+			list : [{
+				title : "Universitas Pendidikan Indonesia",
+				date : "2019 - Sekarang",
+				desc : "Ilmu Komputer"
+			},{
+				title : "Smk Merdeka Bandung",
+				date : "2016 - 2019",
+				desc : "Teknik Komputer dan Jaringan"
+			}]
 		},{
-			title : "Lessons",
-			description : `I have been freelance Web Development for about 6 months.
-			My Goal to become a freelance is to try a project that i have never tried and i 
-			do not think too much about the cost that gives because it has been paid with
-			experience. I feel happy and happy even though sometimes i feel tired.`
+			title : "Pengalaman",
+			type : "list",
+			list : [{
+				title : "Web Developer (Freelance)",
+				date : "2021 - Sekarang",
+				desc : "Freelance"
+			},{
+				title : "Network Technician (Intership)",
+				date : "2018 / 3 Bulan",
+				desc : "PT. Telkom STO Sadang Serang"
+			},{
+				title : "Network Technician (Intership)",
+				date : "2018 / 3 Bulan",
+				desc : "DPRD (Dewan Perwakilan Rakyat Daerah)"
+			}]
 		},{
-			title : "My Portfolio",
-			description : `I have been freelance Web Development for about 6 months.
-			My Goal to become a freelance is to try a project that i have never tried and i 
-			do not think too much about the cost that gives because it has been paid with
-			experience. I feel happy and happy even though sometimes i feel tired.`
+			title : "Kemampuan",
+			type : "list-in-list",
+			listInList : [{
+				title : "Front-End Development",
+				skills : ["HTML","CSS","Javascript","Tailwind","PWA","Webpack"]
+			},{
+				title : "Back-End Development",
+				skills : ["PHP","MonggoDB","MySQL","CodeIgniter","NodeJS"]
+			}]
+		},{
+			title : "Sertifikat",
+			type : "description",
+			description : `Sertifikasi adalah sebuah penetapan yang diberikan oleh organisasi atau asosiasi profesi terhadap seseorang bahwa orang tersebut telah memenuhi standar kompetensi tertentu.
+			- <a href="https://www.pinhome.id" target="_blank">www.pinhome.id</a>`,
+			link : "#/sertifikat"
 		}];
 	}
 	
